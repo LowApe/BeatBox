5278c03737f08bd41c6ed3134fa29274229f6929
LowApe <didiaoyuan@gmail.com>
2017-10-19 15:36:16 +0800

    1.创建ViewHolder 内部类 2.创建 Adapter 内部类 3.创建 SimpleFragmentActivity 模板类，完成 fragment的事务给 fragmentManager

--- app/src/main/java/com/didiaoyuan/blog/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/didiaoyuan/blog/beatbox/BeatBoxActivity.java
@@ -1,13 +1,14 @@
 package com.didiaoyuan.blog.beatbox;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SimpleFragmentActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_beat_box);
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
     }
+
 }
---
+++ app/src/main/java/com/didiaoyuan/blog/beatbox/SimpleFragmentActivity.java
@@ -0,0 +1,36 @@
+package com.didiaoyuan.blog.beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mr.Qu on 2017/9/10.
+ * 这是一个通用的加载Fragment的超类
+ */
+
+public abstract class SimpleFragmentActivity extends AppCompatActivity {
+    //    添加一个抽象方法来生成Fragment
+    protected abstract Fragment createFragment();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        加载Fragment 托管布局
+//        setContentView(R.layout.activity_fragment);
+        setContentView(R.layout.activity_fragment);
+//        初始化FragmentManager
+        FragmentManager fm = getSupportFragmentManager();
+//        获取Fragment的组件
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+//        提交Fragment的事务
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
